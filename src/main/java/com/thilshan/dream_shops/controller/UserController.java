a990065c05f6be153abace88a77e9fa08ef57050
smThilshan <smthilshan@gmail.com>
2025-09-16 11:21:44 +0530

    added user controller

---
+++ src/main/java/com/thilshan/dream_shops/controller/UserController.java
@@ -0,0 +1,64 @@
+package com.thilshan.dream_shops.controller;
+
+import com.thilshan.dream_shops.dto.UserDto;
+import com.thilshan.dream_shops.request.CreateUserRequest;
+import com.thilshan.dream_shops.request.UserUpdateRequest;
+import com.thilshan.dream_shops.response.ApiResponse;
+import com.thilshan.dream_shops.service.user.IUserService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.http.HttpStatus.CONFLICT;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("${api.prefix}/users")
+public class UserController {
+    private final IUserService userService;
+
+    @GetMapping("/{userId}/user")
+    public ResponseEntity<ApiResponse> getUserById(@PathVariable Long userId) {
+        try {
+            var user = userService.getUserById(userId);
+            return ResponseEntity.ok(new ApiResponse("User retrieved successfully", user));
+        } catch (Exception e) {
+            return ResponseEntity.status(NOT_FOUND).body(new ApiResponse("User not found", e.getMessage()));
+        }
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<ApiResponse> createUser(@RequestBody CreateUserRequest request) {
+        try {
+            var user = userService.createUser(request);
+            UserDto userDto  =  userService.convertUserToDto(user);
+            return ResponseEntity.ok(new ApiResponse("User created successfully", userDto));
+        } catch (Exception e) {
+            return ResponseEntity.status(CONFLICT).body(new ApiResponse("Failed to create user", null));
+        }
+    }
+
+    @PutMapping("/{userId}/update")
+    public ResponseEntity<ApiResponse> updateUser( @RequestBody UserUpdateRequest request, @PathVariable Long userId) {
+        try {
+            var updatedUser = userService.updateUser( request, userId);
+            UserDto userDto  =  userService.convertUserToDto(updatedUser);
+            return ResponseEntity.ok(new ApiResponse("User updated successfully", userDto));
+        } catch (Exception e) {
+            return ResponseEntity.status(NOT_FOUND).body(new ApiResponse("User not found", e.getMessage()));
+        }
+    }
+
+    @DeleteMapping("/{userId}/delete")
+    public ResponseEntity<ApiResponse> deleteUser(@PathVariable Long userId) {
+        try {
+            userService.deleteUser(userId);
+            return ResponseEntity.ok(new ApiResponse("User deleted successfully", null));
+        } catch (Exception e) {
+            return ResponseEntity.status(NOT_FOUND).body(new ApiResponse("User not found", null));
+        }
+    }
+
+
+}
